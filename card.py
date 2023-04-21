7af5824d084715fd2998e85e73e6e940b6e250ad
Luís Beck <125098484+LuisHBeck@users.noreply.github.com>
2023-04-21 17:10:16 -0300

    Add files via upload

--- card.py
+++ card.py
@@ -7,10 +7,6 @@ class Card():
     @property
     def name(self):
         return self._name
-    
-    @name.setter
-    def name(self, value):
-        self._name = value
 
     @property
     def tips(self):
