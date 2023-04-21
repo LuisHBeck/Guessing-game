d3ad73dfe969576ee34d45db40d9ee44c4286f00
Luís Beck <125098484+LuisHBeck@users.noreply.github.com>
2023-04-21 16:24:17 -0300

    Add files via upload

---
+++ card_types.py
@@ -0,0 +1,20 @@
+from card import Card
+
+car = Card("Car")
+car.add_tips("Has a windshield",
+            "Has four doors",
+            "Has four wheels",
+            "Has five seats",
+            "Needs a driver",)
+
+motorcycle = Card("Motorcycle")
+motorcycle.add_tips("Has two seats",
+                    "Has two wheels",
+                    "Has no doors",
+                    "You can do a wheelie",
+                    "Randandandandandan")
+
+
+vehicles = [car, motorcycle]
+
+
