5edf2d04dc8b76d73771789561fc1db25db1685f
Luís Beck <125098484+LuisHBeck@users.noreply.github.com>
2023-04-28 16:32:56 -0300

    Add files via upload

--- mqtt/mqtt_publish1.py
+++ mqtt/mqtt_publish1.py
@@ -1,14 +1,18 @@
 import paho.mqtt.client as mqtt
+from paho.mqtt import publish
 from time import sleep
 
 mqttBroker = "10.21.160.16"
-client = mqtt.Client('guessing_game')
-client.connect(mqttBroker)
+port = 1883
+client = mqtt.Client('Client 1')
+client.connect(mqttBroker, port)
 
-tip = "———PRINT TIP———"
+tip = 'Client 1'
+tip1 = "I'm client 1"
+
 
 for c in range(5):
-    client.publish('tip', tip)
-    print('It worked!')
-    sleep(1)
+    publish.single(tip, tip1, hostname=mqttBroker, port=port)
+    print('It worked! 1°')
+    sleep(10)
 
