8f2a0d9eb23b85aac38bc7394107e38bef7edda8
Luís Beck <125098484+LuisHBeck@users.noreply.github.com>
2023-04-28 09:30:01 -0300

    Update card.py

--- card.py
+++ card.py
@@ -4,7 +4,7 @@ class Card():
     def __init__(self, name) -> None:
         self._name = name
         self._tips = []
-        self._life = 5
+        # self._life = 5
 
     @property
     def name(self):
@@ -18,27 +18,26 @@ class Card():
         for tip in tips:
             self._tips.append(tip)
 
-    def lose_file(self, value=1):
-        return self._life - value
+    # def lose_file(self, value=1):
+    #     return self._life - value
     
     def print_tips(self, z):
         os.system("cls")
         print(f'Type #{z+1}: {self.tips[z]}')
         
-    def verify_answer(self, answer):
+    def verify_answer(self, answer, life):
         if answer == self.name:
             print("Congrats buddy!! You won!")
             quit()
         else:
-            self._life -= 1
+            life -= 1
+            
 
     def play(self):
         z = 0
-        while self._life > 0:
+        life = 5
+        while life > 0:
             self.print_tips(z)
             z += 1
             answer = str(input("Input your answer>> ")).capitalize()
-            self.verify_answer(answer)
-            
-                
-            
+            self.verify_answer(answer, life)
